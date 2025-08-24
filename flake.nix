081cccfb715ed1c40d1d3e8b59c53248fb1c14f7
meowesque <deandrade@posteo.com>
2025-08-24 14:55:49 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk { };
+        
+        buildInputs = with pkgs; [ ];
+
+        nativeBuildInputs = with pkgs; [ ];
+      in
+      rec {
+        defaultPackage = packages.server;
+        packages =
+          {
+            server = naersk'.buildPackage {
+              src = ./.;
+              nativeBuildInputs = nativeBuildInputs;
+              buildInputs = buildInputs;
+            };
+            container = pkgs.dockerTools.buildImage
+              {
+                name = "server";
+                config = {
+                  entrypoint = [ "${packages.server}/bin/server" ];
+                };
+              };
+          };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;
+            [
+              nixpkgs-fmt
+              cmake
+              rustc
+              cargo
+              clippy
+              rust-analyzer 
+              nixfmt
+            ] ++ buildInputs ++ nativeBuildInputs;
+        };
+      }
+    );
+}
